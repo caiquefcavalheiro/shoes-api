94f00989c3ba91656f639f4e383aa1b04b6e9597
Caique F. Cavalheiro <caiquefcavalheiro@gmail.com>
2023-08-03 22:04:45 -0300

    feat: add params to shoes get

--- shoes/views.py
+++ shoes/views.py
@@ -1,11 +1,23 @@
 from rest_framework import generics
 from .models import Shoes
 from .serializers import ShoesSerializer
+from rest_framework.response import Response
 
 
 class ShoesView(generics.ListCreateAPIView):
     queryset = Shoes.objects.all()
     serializer_class = ShoesSerializer
+
+    def get(self, request, *args, **kwargs):
+        search_params = request.query_params.get("search")
+
+        if search_params:
+            shoes = Shoes.objects.filter(name=search_params)
+
+            serializer = ShoesSerializer(shoes, many=True)
+            return Response(serializer.data)
+
+        return super().get(request, *args, **kwargs)
 
 
 class ShoewViewId(generics.RetrieveUpdateDestroyAPIView):
